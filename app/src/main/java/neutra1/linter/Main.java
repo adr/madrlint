b782576a561de1ad5f481f26580fcb1e23a3f5d8
Neutra1l <viethung2012003@gmail.com>
2026-09-01 13:35:00 +0700

    Version bump

--- app/src/main/java/neutra1/linter/Main.java
+++ app/src/main/java/neutra1/linter/Main.java
@@ -42,7 +42,7 @@ import picocli.CommandLine.Parameters;
     description = "Lint MADR files",
     mixinStandardHelpOptions = true,
     customSynopsis = "madrlint [-hOqV] [-n <rule>[,rule...]] [-o <outputFile>] [--output-format <format>] <madrFile>",
-    version="1.0.0"
+    version="1.1.0"
 )
 public class Main implements Runnable {
 
